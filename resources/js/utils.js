ee607fea67b9b373f9b99e9a113e19790c05e311
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-05-23 13:00:50 -0500

    adds 'makeEmbedLinkYT' function

---
+++ resources/js/utils.js
@@ -0,0 +1,7 @@
+export const makeEmbedLinkYT = url => {
+  const videoCode = url
+    .split("/")
+    .find(s => s.includes("watch"))
+    .substring(8, 19);
+  return `https://youtube.com/embed/${videoCode}`;
+}
