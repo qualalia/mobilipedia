d4352d69247e98c7245aa02e3dbbef84262bc132
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-05-27 14:38:13 -0500

    Add origin query to youtube embed link.

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -3,5 +3,5 @@ export const makeEmbedLinkYT = url => {
     .split("/")
     .find(s => s.includes("watch"))
     .substring(8, 19);
-  return `https://youtube.com/embed/${videoCode}`;
+  return `https://youtube.com/embed/${videoCode}?origin=https://localhost:8080`;
 }
