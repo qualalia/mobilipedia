4e1de71845a3423b4a4d218d8e2e2690ed306055
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-05-27 14:36:14 -0500

    Add 'tags' and 'description' fields

--- database/migrations/2020_05_22_180521_create_links_table.php
+++ database/migrations/2020_05_22_180521_create_links_table.php
@@ -16,6 +16,8 @@ class CreateLinksTable extends Migration
     Schema::create('links', function (Blueprint $table) {
       $table->id();
       $table->string('url')->unique();
+      $table->text('description')->nullable($value = true);
+      $table->text('tags')->default('uncategorized,');
       $table->timestamps();
     });
   }
