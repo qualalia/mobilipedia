2ad42960b135a2d9d2251f653edb3fdd3696af8c
Frances Tiffin <ftiffin@alumnae.mills.edu>
2020-05-27 14:37:29 -0500

    Export refactored components from their (new) files.

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -2,3 +2,5 @@ export { default as Home } from "./Home.js";
 export { default as Routes } from "./Routes.js";
 export { default as AddNewLink } from "./AddNewLink.js";
 export { default as LinksContainer } from "./LinksContainer.js";
+export { default as SingleLink } from "./SingleLink.js";
+export { default as LinksList } from "./LinksList.js";
